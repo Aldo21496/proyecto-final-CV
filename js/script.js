ad87f5a4fe36a19128a411d1fef2fbea499b8814
Reinaldo Carlos Romano <aldo21406@gmail.com>
2023-02-25 20:17:51 -0300

    Primer commit del proyecto de Curriculum Vitae

---
+++ js/script.js
@@ -0,0 +1,13 @@
+window.onload=function(){
+  alert('Bienvenido a esta página.');
+}
+
+function changeColor(newColor) {
+  var elem = document.getElementById('para');
+  elem.style.color = newColor;
+}
+
+
+function popup_descarga(){
+  alert('Todavia no esta implementado!');
+}
